529ddceb8e39f5e9977c80dc7fe350edef51de29
unknown <1daw3@HZ114825.H012108.NET>
2022-05-10 11:28:03 +0200

    sigo con problemas

--- src/datos/DataCSV.cs
+++ src/datos/DataCSV.cs
@@ -22,38 +22,63 @@ namespace datos
 
         public List<Cliente> leer()
         {
-            List<Cliente> misClientes = new(){
+            List<Cliente> misClientes = new();
                 var data = File.ReadAllLines(_fileClientes).Where(row => row.Length > 0).ToList();
                 data.ForEach(row =>
                 {
                     var campos = row.Split(",");
                     Cliente cliente = new Cliente
                     (
-                        IDPedido : campos[0],
+                        IDPedido : int.Parse(campos[0]),
                         Nombre : campos[1],
                         Apellido : campos[2],
                         DNI : campos[3],
                         Direccion : campos[4]
-                    )
+                    );
                     misClientes.Add(cliente);
                 });
 
                 return misClientes;
-            }
+            
+        }
+    }
+    public class PedidosCSV : IData<Pedido>
+    {
+        string _filePedidos = "../../RepositoriosCSV/pedidos.csv";
+
+        public void guardar(List<Pedido> misPedidos)
+        {
+            List<string> data = new(){ };
+            misPedidos.ForEach(Pedido =>
+            {
+                var str =$"{Cliente.idCliente},{Cliente.nombre},{Cliente.apellido},{Cliente.dni},{Cliente.direccion}";
+                data.Add(str);
+            });
+            File.WriteAllLines(_filePedidos, data);
+
         }
 
+        public List<Pedido> leer()
+        {
+            List<Pedido> misClientes = new();
+                var data = File.ReadAllLines(_filePedidos).Where(row => row.Length > 0).ToList();
+                data.ForEach(row =>
+                {
+                    var campos = row.Split(",");
+                    Pedido pedido = new Pedido
+                    (
+                        IDPedido : int.Parse(campos[0]),
+                        Nombre : campos[1],
+                        Apellido : campos[2],
+                        DNI : campos[3],
+                        Direccion : campos[4]
+                    );
+                    misPedidos.Add(Pedido);
+                });
 
-
+                return misPedidos;
+            
+        }
     }
-
-
-
-
-
-
-
-
-
-
 
 }
